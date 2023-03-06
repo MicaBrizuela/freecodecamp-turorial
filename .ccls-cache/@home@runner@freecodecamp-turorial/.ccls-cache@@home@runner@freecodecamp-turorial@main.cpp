839d8f048601f63ce18ffd8eef251aadae04a57d
mbrizuela <102945330+MicaBrizuela@users.noreply.github.com>
2023-03-06 01:55:12 +0000

    clarified a question and read other files

--- .ccls-cache/@home@runner@freecodecamp-turorial/.ccls-cache@@home@runner@freecodecamp-turorial@main.cpp
+++ .ccls-cache/@home@runner@freecodecamp-turorial/.ccls-cache@@home@runner@freecodecamp-turorial@main.cpp
@@ -10,13 +10,9 @@ int main() {
   //order : PEMDAS
   //potencia exponente multiplicacion division suma resta
   int numero = 2;
-  numero++; // numero + 1
-  numero--; // numero - 1
-  numero += 80; // numero + 80
-  //tambien se puede con -=, *=, /= investigar porque no entendi:
-  // se refiere a esto como una abreviacion de la otra forma
-  // numero *= 5 = numero = numero * 5
-  // numero /= 2 = numero = numero / 5
-  
+  numero++;
+  numero--;
+  numero += 80; 
+  //tambien se puede con -=, *=, /= investigar porque no entendi
   return 0;
 }
--- .ccls-cache/@home@runner@freecodecamp-turorial/main.cpp
+++ .ccls-cache/@home@runner@freecodecamp-turorial/main.cpp
@@ -7,12 +7,16 @@ int main() {
   cout << 8 * 2 << "\n";
   cout << 8 / 2 << "\n";
   cout << 8 % 3 << endl; //returns the remainder of the division
-  //order : PEMDA3
+  //order : PEMDAS
   //potencia exponente multiplicacion division suma resta
   int numero = 2;
-  numero++;
-  numero--;
-  numero += 80; 
-  //tambien se puede con -=, *=, /= investigar porque no entendi
+  numero++; // numero + 1
+  numero--; // numero - 1
+  numero += 80; // numero + 80
+  //tambien se puede con -=, *=, /= investigar porque no entendi:
+  // se refiere a esto como una abreviacion de la otra forma
+  // numero *= 5 = numero = numero * 5
+  // numero /= 2 = numero = numero / 5
+  
   return 0;
 }
